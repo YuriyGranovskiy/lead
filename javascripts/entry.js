da4c5fb64222de609e774a61ecd3329d334fd17c
Yuriy Granovskiy <yuriy.granovskiy@gmail.com>
2017-05-18 02:13:16 +0300

    Initial commit

---
+++ javascripts/entry.js
@@ -0,0 +1,6 @@
+require('../less/main.less');
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<div className="myDiv">Enter your notes</div>, document.getElementById('content'));
---
+++ main.js
@@ -0,0 +1,22 @@
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+let mainWindow;
+
+require('electron').crashReporter.start(
+    {
+        "companyName": "Grain",
+        "submitURL": "http://localhost:8088/cr"
+    }
+);
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 340, height: 500});
+  mainWindow.loadURL('file://' + __dirname + '/public/index.html');
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
