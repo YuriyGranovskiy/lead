4ae17a4355ae77b0c578e508eb5ea79cb8c5282b
Yuriy Granovskiy <yuriy.granovskiy@gmail.com>
2017-05-19 01:10:34 +0300

    Add NodeList stub

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -3,4 +3,23 @@ require('../less/main.less');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<div className="myDiv">Enter your notes</div>, document.getElementById('content'));
+
+class NoteList extends React.Component {
+    renderItemOrEditField(item) {
+        return (<li
+            key={item.id}
+            className="list-group-item">
+            {item.value}
+        </li>);
+    };
+    render() {
+        return (<ul className="list-group" style={{ listStyleType: "none" }}>{this.props.items.map((item) => {
+            return this.renderItemOrEditField(item);
+        })}</ul>);
+    };
+};
+
+ReactDOM.render(<div className="myDiv"><div>Enter your notes</div>
+    <div id="note-list"></div></div>, document.getElementById('content'));
+
+ReactDOM.render(<NoteList items={[{ id: 1, value: "Value1" }, { id: 2, value: "Value2" }]} />, document.getElementById('note-list'));
--- main.js
+++ main.js
@@ -16,6 +16,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 340, height: 500});
   mainWindow.loadURL('file://' + __dirname + '/public/index.html');
+  // mainWindow.openDevTools();
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
